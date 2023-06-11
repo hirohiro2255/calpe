4d16afef3a597effdcbad2b8a81de0cd544f7a44
Hirokazu Hirono <hirono.hirokazu@gmail.com>
2023-06-11 23:35:05 +0900

    Bump up to 0.0.5

--- src/lib/components/Calpe.svelte
+++ src/lib/components/Calpe.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
+	import Lexer from '$lib/utils/lexer';
+	import { Parser, Calculator } from '$lib/utils/calculator';
 	let isFocused = false;
+	let state = '';
 
 	function onFocus() {
 		isFocused = true;
 	}
-	function outOfFocus() {
+	function closeCalc() {
 		isFocused = false;
+	}
+
+	function handleResult() {
+		const lexer = new Lexer(state);
+		const parser = new Parser(lexer);
+		const calc = new Calculator(parser);
+		state = calc.cc().toString();
 	}
 </script>
 
@@ -13,29 +23,33 @@
 {#if isFocused}
 	<section class="calpe-container">
 		<div class="screen-container">
-			<p class="screen-result">0</p>
+			<p class="screen-result">{state.length === 0 ? '0' : state}</p>
 		</div>
 		<div class="keypad-container">
 			<div class="keypad-wrapper">
-				<button>7</button>
-				<button>8</button>
-				<button>9</button>
-				<button>C</button>
-				<button>4</button>
-				<button>5</button>
-				<button>6</button>
-				<button>+</button>
-				<button>1</button>
-				<button>2</button>
-				<button>3</button>
+				<button on:click={() => (state = state + '7')}>7</button>
+				<button on:click={() => (state = state + '8')}>8</button>
+				<button on:click={() => (state = state + '9')}>9</button>
+				<button
+					on:click={() => (state = state.length === 0 ? '' : state.slice(0, state.length - 1))}
+					>C</button
+				>
+				<button on:click={() => (state = state + '4')}>4</button>
+				<button on:click={() => (state = state + '5')}>5</button>
+				<button on:click={() => (state = state + '6')}>6</button>
+				<button on:click={() => (state = state.length === 0 ? '' : state + '+')}>+</button>
+				<button on:click={() => (state = state + '1')}>1</button>
+				<button on:click={() => (state = state + '2')}>2</button>
+				<button on:click={() => (state = state + '3')}>3</button>
 				<button>-</button>
 				<button>.</button>
-				<button>0</button>
+				<button on:click={() => (state = state + '0')}>0</button>
 				<button>/</button>
 				<button>x</button>
 				<button>AC</button>
-				<button>=</button>
+				<button on:click={handleResult}>=</button>
 			</div>
+			<button class="close-button" on:click={closeCalc}>Close</button>
 		</div>
 	</section>
 {/if}
@@ -65,5 +79,6 @@
 		display: grid;
 		grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
 		grid-template-columns: 1fr 1fr 1fr 1fr;
+		gap: 3px;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Calpe from '$lib';
+	// import Calpe from 'calpe';
 </script>
 
 <Calpe />
